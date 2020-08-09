5ab32cec6d72872f18db048159fff25338d8baff
Karan Chawla <karan.chawla.19@ucl.ac.uk>
2020-08-09 23:52:58 +0100

    Stocks Time-Series

---
+++ plot_stocks_timeseries.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import preprocess as pre
+import matplotlib.pyplot as plt
+from matplotlib.dates import AutoDateFormatter, AutoDateLocator
+import seaborn as sns
+
+def plot_stocks_timeseries():
+    df = pre.preprocess()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    xtick_locator = AutoDateLocator()
+    xtick_formatter = AutoDateFormatter(xtick_locator)
+
+    df["UNH"].plot()
+    df["NKE"].plot()
+    ax.xaxis.set_major_locator(xtick_locator)
+    ax.xaxis.set_major_formatter(xtick_formatter)
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_stocks_timeseries()
